7b756fb2b5891e09aba6a2fdfee1d620d832fa41
Alejandro <alejandropalharescampos@gmail.com>
2022-07-11 17:55:42 -0300

    a

---
+++ diad.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velociadade do seu carro é ${vel}km/h`);
+if (vel > 60) {
+    console.log('voçe foi multado por passa do limite');
+}
+console.log('dirige sempre usando sinto de segurança');
